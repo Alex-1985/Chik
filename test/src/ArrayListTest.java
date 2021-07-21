71b3f70143bb16e56cade8ea11a82606325ec10c
RealChik <werhovna@gmail.com>
2021-07-22 00:40:27 +0300

    Created class for testing in project Chik. Added first tests. Played with syntax.

--- test/src/ArrayListTest.java
+++ test/src/ArrayListTest.java
@@ -1,4 +1,4 @@
-package org.chik;
+//package org.chik;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
@@ -24,6 +24,23 @@ public class ArrayListTest<pubic> {
     }
 
     @Test
+    public void testAdd1() {        //are tests always 'void'?
+        list.add("elem1");
+        list.add("elem2");
+        list.add(0,"elem0");
+        int idx;
+        idx = 0;
+        System.out.println("The "+idx+"th element is "+list.get(idx)+", right?");
+//        assertTrue("Incorrect sequence", list.get(0) = "elem0");      //dont work, return later
+//        assertTrue("Incorrect sequence", list.get(0) = list.get(0));      //wrong condition? variable where?
+        assertEquals("This is wrong element","elem0", list.get(idx));
+        assertTrue("There is no such element", list.contains("elem0"));
+//        assertTrue("There is no such element",list.contains("elem10"));
+        assertFalse("The element is present", list.contains("elem101"));
+//        assertFalse("The element is present",list.contains("elem1"));
+    }
+
+    @Test
     public void testClear() {
         list.add("elem1");
         list.add("elem2");
@@ -32,15 +49,5 @@ public class ArrayListTest<pubic> {
         list.clear();
         assertEquals("Incorrect size of the list ", 0, list.size());
     }
-    @Test
-    public void testAdd1() {        //are tests always 'void'?
-        list.add("elem1");
-        list.add("elem2");
-        list.add(0,"elem0");
-//        assertTrue("Incorrect sequence","elem0",list.get(0));    //dont work, return later
-        assertTrue("There is no such element",list.contains("elem0"));
-        assertTrue("There is no such element",list.contains("elem10"));
-        assertFalse("The element is present",list.contains("elem101"));
-        assertFalse("The element is present",list.contains("elem1"));
-    }
+
 }
